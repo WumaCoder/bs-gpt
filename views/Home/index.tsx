f3a3c93a88c336e039ab605c6a0a60982a663473
WumaCoder <1829913225@qq.com>
2024-01-07 21:54:09 +0800

    fix: 优化代码逻辑

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,5 +1,10 @@
-import React, { useEffect } from "react";
-import Chat, { Bubble, MessageProps, useMessages } from "@chatui/core";
+import React, { useEffect, useRef, useState } from "react";
+import Chat, {
+  Bubble,
+  MessageProps,
+  QuickReplyItemProps,
+  useMessages,
+} from "@chatui/core";
 import "@chatui/core/dist/index.css";
 import { BsSdk } from "@/libs/bs-sdk/BsSdk";
 import { BsSql } from "@/libs/bs-sql";
@@ -9,6 +14,27 @@ const bsSdk = new BsSdk({});
 const bsSql = new BsSql(bsSdk);
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [quickReplies] = useState<QuickReplyItemProps[]>([
+    {
+      name: "查询当前表",
+      code: "select * from ?",
+      // isNew: true,
+      // isHighlight: true,
+    },
+    {
+      name: "帮助",
+      code: "help",
+      // isNew: true,
+      // isHighlight: true,
+    },
+    {
+      name: "联系开发者",
+      code: "feedback",
+      // isNew: true,
+      // isHighlight: true,
+    },
+  ]);
   const { messages, appendMsg, setTyping, updateMsg, deleteMsg, resetList } =
     useMessages([]);
 
@@ -32,29 +58,46 @@ export default function Home() {
       setTyping(true);
       userSend("text", val);
 
-      if (val.startsWith("token ")) {
-        const [key, v] = val.split(" ");
-        bsSdk.storage.set(key, v).then(() => {
-          botSend("text", "设置成功");
-        });
-      } else if (val.startsWith("token")) {
-        const [key] = val.split(" ");
-        bsSdk.storage.get(key).then((v) => {
-          botSend("text", v || "未设置");
-        });
-      } else if (val.startsWith("clear")) {
-        resetList([
-          {
-            _id: geneId(),
-            type: "text",
-            content:
-              "欢迎使用 GPT查询 助手，您可以在下方输入框进行对话式查询多维表格数据，比如：\n\n帮我查一下每天销售量。",
-          },
-        ]);
-      } else if (val.startsWith("select")) {
-        select(val).then(async (content) => {
-          botSend("select", content);
-        });
+      const [cmd, ...args] = parseStringCmd(val);
+
+      switch (cmd) {
+        case "token":
+          if (args.length) {
+            bsSdk.storage.set("token", args[0]).then(() => {
+              botSend("text", "设置成功");
+            });
+          } else {
+            bsSdk.storage.get("token").then((v) => {
+              botSend("text", v || "未设置");
+            });
+          }
+          break;
+        case "select":
+          select(val).then(async (content) => {
+            botSend("select", content);
+          });
+          break;
+        case "help":
+          botSend(
+            "text",
+            "您可以在下方输入框进行对话式查询多维表格数据，比如：\n\n帮我查一下每天销售量。"
+          );
+          break;
+        case "feedback":
+          botSend("text", "请联系微信：zhangyunchen1992");
+          break;
+        case "clear":
+          resetList([
+            {
+              _id: geneId(),
+              type: "text",
+              content:
+                "欢迎使用 GPT查询 助手，您可以在下方输入框进行对话式查询多维表格数据，比如：\n\n帮我查一下每天销售量。",
+            },
+          ]);
+          break;
+        default:
+          break;
       }
 
       // setTimeout(() => {
@@ -115,12 +158,18 @@ export default function Home() {
     return <Bubble content={content} />;
   }
 
+  function handleQuickReplyClick(e: any) {
+    handleSend("text", e.code);
+  }
+
   return (
     <Chat
       navbar={{ title: "GPT 查询" }}
       messages={messages}
       renderMessageContent={renderMessageContent}
       onSend={handleSend}
+      quickReplies={quickReplies}
+      onQuickReplyClick={handleQuickReplyClick}
     />
   );
 }
@@ -156,4 +205,32 @@ async function select(sql: string) {
     columns,
     result,
   };
+}
+
+function parseStringCmd(str: string) {
+  const args = [];
+  let temp = "";
+  let begin = false;
+  let isQuote = false;
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i];
+    if (c === " " && temp) {
+      args.push(temp);
+      temp = "";
+    } else if (c === '"' && !begin) {
+      begin = true;
+    } else if (c === '"' && begin) {
+      begin = false;
+    } else if (c === "\\" && begin) {
+      temp += '"';
+      i += 2;
+    } else if (c !== " " || begin) {
+      temp += c;
+    }
+  }
+  if (temp) {
+    args.push(temp);
+  }
+  return args;
 }
